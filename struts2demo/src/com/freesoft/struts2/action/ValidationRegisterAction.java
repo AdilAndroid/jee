1718ea4fe7de526314104417459800344e584bd7
sandy <jariwsz@qq.com>
2013-03-05 15:26:37 +0800

    Add struts2demo: customize error message of datatype converter.

--- struts2demo/src/com/freesoft/struts2/action/ValidationRegisterAction.java
+++ struts2demo/src/com/freesoft/struts2/action/ValidationRegisterAction.java
@@ -27,33 +27,33 @@ public class ValidationRegisterAction extends ActionSupport {
 	public void validate() {
 		System.out.println("validate.");
 		
-		if (username == null || username.length() < 4 || username.length() > 6) {
-			addFieldError("username", "用户名不合法，用户名长度必须在4~6之间。");
-		} 
-		if (password == null || repassword == null || password.length() < 4 || password.length() >8 || repassword.length() < 4 || repassword.length() > 8) {
-			addFieldError("password", "密码不合法，密码长度必须在4~8之间。");
-		} else if (!password.equals(repassword)) {
-			addFieldError("password", "两次密码不同。");
-		} 
-		if (age < 0 | age > 100) {
-			addFieldError("age", "年龄必须在0~100之间。");
-		} 
-		if (birthday == null) {
-			addFieldError("birthday", "年龄不能为空。");
-		}
-		if (graduation == null) {
-			addFieldError("graduation", "毕业日期不能为空。");
-		} else{
-			Calendar c1 = Calendar.getInstance();
-			c1.setTime(birthday);
-			
-			Calendar c2 = Calendar.getInstance();
-			c2.setTime(graduation);
-			
-			if (c1.after(c2)) {
-				addFieldError("graduation", "毕业日期不能为空。");
-			}
-		}
+//		if (username == null || username.length() < 4 || username.length() > 6) {
+//			addFieldError("username", "用户名不合法，用户名长度必须在4~6之间。");
+//		} 
+//		if (password == null || repassword == null || password.length() < 4 || password.length() >8 || repassword.length() < 4 || repassword.length() > 8) {
+//			addFieldError("password", "密码不合法，密码长度必须在4~8之间。");
+//		} else if (!password.equals(repassword)) {
+//			addFieldError("password", "两次密码不同。");
+//		} 
+//		if (age < 0 | age > 100) {
+//			addFieldError("age", "年龄必须在0~100之间。");
+//		} 
+//		if (birthday == null) {
+//			addFieldError("birthday", "年龄不能为空。");
+//		}
+//		if (graduation == null) {
+//			addFieldError("graduation", "毕业日期不能为空。");
+//		} else{
+//			Calendar c1 = Calendar.getInstance();
+//			c1.setTime(birthday);
+//			
+//			Calendar c2 = Calendar.getInstance();
+//			c2.setTime(graduation);
+//			
+//			if (c1.after(c2)) {
+//				addFieldError("graduation", "毕业日期不能为空。");
+//			}
+//		}
 	}
 
 	public String getUsername() {
