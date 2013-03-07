deb7349a4694b818fd09627fe05144e5c815f666
sandy <jariwsz@qq.com>
2013-03-07 17:32:15 +0800

    Add struts2demo: result type.

---
+++ struts2demo/src/com/freesoft/struts2/action/ResultTypeLogin2Action.java
@@ -0,0 +1,42 @@
+package com.freesoft.struts2.action;
+
+import java.util.Date;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ResultTypeLogin2Action extends ActionSupport {
+	private String username;
+	private String password;
+	private int age;
+	private Date birthday;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
