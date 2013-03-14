2a13212f66287fe6455a4cdf3e0e81851a3eaa5d
sandy <jariwsz@qq.com>
2013-03-14 14:17:38 +0800

    Add struts2demo:download file with filename & resolve charset problem.

---
+++ struts2demo/src/com/freesoft/struts2/action/DownloadFileIncludeFileNameAction.java
@@ -0,0 +1,45 @@
+package com.freesoft.struts2.action;
+
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class DownloadFileIncludeFileNameAction extends ActionSupport {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9192890264761613759L;
+	private String filename;
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+	public String getFilename() {
+		try {
+			filename=new String(filename.getBytes(),"ISO-8859-1");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	
+	public InputStream getDownloadFile() {
+//		try {
+//			filename = new String(filename.getBytes(), "ISO8859-1");
+//		} catch (UnsupportedEncodingException e) {
+//			e.printStackTrace();
+//		}
+		return ServletActionContext.getServletContext().getResourceAsStream("/upload/" + filename);
+	}
+
+}
